9f26b85d3c0d50e47d90614046849ec1b9a1d49c
Abraham Pena <pena.abe@gmail.com>
2018-06-07 13:40:32 -0500

    added script for basic flask application scaffolding

---
+++ flask_startup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#A bash script to create the basic scaffolding for a flask project all in one go
+
+mkdir {templates,static} | touch {app.py,templates/index.html,templates/layout.html,static/style.css,static/app.js}
